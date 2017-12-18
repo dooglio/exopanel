9014755d8cbecedf7243b7abbc25be6b228a2946
R. Douglas Barbieri <doug@dooglio.net>
2017-12-17 23:34:50 -0800

    Center at the top of the primary display. Removed window chrome.

--- app.js
+++ app.js
@@ -1,21 +1,38 @@
-const {app, BrowserWindow} = require('electron') // http://electron.atom.io/docs/api
-const path = require('path')         // https://nodejs.org/api/path.html
-const url = require('url')           // https://nodejs.org/api/url.html
+const electron = require('electron')  // http://electron.atom.io/docs/api
+const {app, BrowserWindow} = electron // http://electron.atom.io/docs/api
+const path = require('path')          // https://nodejs.org/api/path.html
+const url = require('url')            // https://nodejs.org/api/url.html
 
 let window = null
 
 // Wait until the app is ready
 app.once('ready', () => {
+
+  const {width, height } = electron.screen.getPrimaryDisplay().workAreaSize
+  const win_width      = 400
+  const win_height     = 500
+
+  const win_x = (width/2) - win_width
+
+  console.log( "width =" + width + ", height=" + height + ", win_x = " + win_x )
+
   // Create a new window
   window = new BrowserWindow({
+    alwaysOnTop: true,
+    frame: false,
+    titleBarStyle: 'hidden',
+    x: win_x,
+    y: 0,
     // Set the initial width to 400px
-    width: 400,
+    width: win_width,
     // Set the initial height to 500px
-    height: 500,
+    height: win_height,
     // Don't show the window until it ready, this prevents any white flickering
     show: false,
     // Don't allow the window to be resized.
     resizable: false,
+
+    backgroundColor: 'black',
   })
 
   // Load a URL in the window to the local index.html path
